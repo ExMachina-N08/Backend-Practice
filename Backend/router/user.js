7938156f81174b2532255b504b396a6282444f94
ExMachina-N08 <aaron.le1995@gmail.com>
2024-10-08 19:57:52 +0700

    frontend update

--- Backend/router/user.js
+++ Backend/router/user.js
@@ -16,10 +16,28 @@ router.post("/register", createUser);
 router.post("/login", loginUser);
 
 // User profile routes
-router.get("/:id/profile", authentication, (req, res) => {
-  const showDetail = req.query.showDetail;
-  const message = `Welcome to profile page, User ${req.user.username}`;
-  if (showDetail === "true") res.status(200).json({ message });
+router.get("/:id/profile", authentication, async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    // Find the user by ID
+    const user = await User.findById(userId).select("username role email name"); // Add fields to select
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const showDetail = req.query.showDetail;
+    const message = `Welcome to profile page, User ${user.username}`;
+    if (showDetail === "true") {
+      return res.status(200).json({ message, role: user.role, user });
+    }
+
+    return res.status(200).json({ message });
+  } catch (error) {
+    console.error("Error fetching user profile:", error);
+    res.status(500).json({ message: "Error fetching user profile" });
+  }
 });
 
 // Update and delete user routes
